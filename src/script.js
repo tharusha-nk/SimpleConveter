8d3171d7262349827d3a9b51147bd14646aee5ac
Tharusha Navanjana <84829909+tharusha-nk@users.noreply.github.com>
2025-03-27 20:37:53 +0530

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,58 @@
+const kgtog = document.getElementById("kgtog");
+const gtokg = document.getElementById("gtokg");
+const mtocm = document.getElementById("mtocm");
+const cmtom = document.getElementById("cmtom");
+const ctof = document.getElementById("ctof");
+const ftoc = document.getElementById("ftoc");
+let answeight = 0;
+let anslength = 0;
+let anstemp = 0;
+
+kgtog.onclick = function () {
+    weight = Number(document.getElementById("weight").value);
+
+    const answeigth = weight * 1000;
+    document.getElementById("answeight").textContent = answeigth + `g`;
+
+}
+
+
+gtokg.onclick = function () {
+    weight = Number(document.getElementById("weight").value);
+
+    const answeigth = weight / 1000;
+    document.getElementById("answeight").textContent = answeigth + `kg`;
+
+}
+
+mtocm.onclick = function () {
+    length = Number(document.getElementById("length").value);
+
+    const anslength = length * 100;
+    document.getElementById("anslength").textContent = anslength + `cm`;
+
+}
+
+cmtom.onclick = function () {
+    length = Number(document.getElementById("length").value);
+
+    const anslength = length / 100;
+    document.getElementById("anslength").textContent = anslength + `m`;
+
+}
+
+ctof.onclick = function () {
+    temperature = Number(document.getElementById("temperature").value);
+
+    const anstemp = (temperature * 9 / 5) + 32;
+    document.getElementById("anstemp").textContent = anstemp + `°F`;
+
+}
+
+ftoc.onclick = function () {
+    temperature = Number(document.getElementById("temperature").value);
+
+    const anstemp = (temperature - 32) * 5 / 9;
+    document.getElementById("anstemp").textContent = anstemp + `°C`;
+
+}
